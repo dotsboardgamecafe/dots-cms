340f1c74d9368f0ed7b7a05fcba7f3a4b790113c
fide-developer <faadjarfirdaus@gmail.com>
2024-05-26 18:00:08 +0700

    Feat: Added the edit banner modal component

---
+++ src/components/PageComponents/BannerPage/Modals/EditBannerModal.tsx
@@ -0,0 +1,40 @@
+import { PropsWithRef } from 'react';
+
+import { updateBanners } from '@/lib/api/banner';
+
+import AddBannerForm from '@/components/PageComponents/BannerPage/Forms/AddBannerForm';
+import { Modal, ModalContent, ModalHeader, ModalTitle } from '@/components/ui/Modal';
+import { Separator } from '@/components/ui/Separator';
+import Typography from '@/components/ui/Typography';
+
+import { TBannerData, TPostBannerPayload } from '@/types/banner';
+
+type Props = PropsWithRef<{
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  bannerData?: TBannerData
+}>;
+
+const EditBannerModal = ({ open, onOpenChange, bannerData }: Props) => {
+  function handleUpdateBanner(data: TPostBannerPayload) {
+    return updateBanners({ param: bannerData?.banner_code, body: { ...data, name: data.title } })
+  }
+
+  return (
+    <Modal open={open} onOpenChange={onOpenChange} >
+      <ModalContent hideCloseIcon>
+        <ModalHeader>
+          <ModalTitle>
+            <Typography variant='heading-h4'>
+              Edit Banner
+            </Typography>
+          </ModalTitle>
+        </ModalHeader>
+        <Separator />
+        <AddBannerForm onClose={() => onOpenChange(false)} defaultData={bannerData} onSubmit={handleUpdateBanner} />
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default EditBannerModal;

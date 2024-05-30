7a103e6bb84ec9cc94a71fac2a9b956f28bad649
fide-developer <faadjarfirdaus@gmail.com>
2024-05-30 23:44:20 +0700

    Feat: Add snake case to string helper

--- src/helper/string.ts
+++ src/helper/string.ts
@@ -1,3 +1,12 @@
-export const currencyFormat = ( value: number ) => {
-  return new Intl.NumberFormat( 'id-ID', { style: 'currency', currency: 'IDR' } ).format( value ).replace( /(\.|,)00$/g, '' );
+export const currencyFormat = (value: number) => {
+  return new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(value).replace(/(\.|,)00$/g, '');
 };
+
+export function snakeCaseToString(data?: string): string {
+  if (!data || typeof data !== 'string') return ''
+
+  return data.split('_').map((partString) => {
+    const [firstLetter, ...restString] = partString.toLowerCase()
+    return firstLetter.toUpperCase() + restString.join('')
+  }).join(' ')
+}

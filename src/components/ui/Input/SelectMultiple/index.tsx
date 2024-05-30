4fd487b388900c1ac38e093f5e209d2351e0517b
fide-developer <faadjarfirdaus@gmail.com>
2024-05-30 23:40:17 +0700

    Feat: Adjust select multiple component

--- src/components/ui/Input/SelectMultiple/index.tsx
+++ src/components/ui/Input/SelectMultiple/index.tsx
@@ -1,66 +1,132 @@
 'use client';
-import React from 'react';
-import Select from 'react-select';
+import React, { memo, PropsWithChildren, ReactNode, useCallback, useRef } from 'react';
+import Select, { components, OptionProps, Options, ValueContainerProps } from 'react-select';
 
+import { cn } from '@/lib/utils';
+
+import Checkbox from '@/components/ui/Input/Checkbox';
 import InputWrapper from '@/components/ui/Input/InputWrapper';
 
+export type SelectOptionType = {
+  label: string,
+  value: string
+}
+
 const SelectMultiple = React.forwardRef<
-  React.ElementRef<typeof Select>,
-  React.ComponentPropsWithoutRef<typeof Select>
+  React.ElementRef<typeof Select<SelectOptionType, true>>,
+  React.ComponentPropsWithoutRef<typeof Select<SelectOptionType, true>>
 >
-  ( ( { ...props }, ref ) => (
+  (({ components, ...props }, ref) => (
     <InputWrapper >
-      <Select { ...props }
-        ref={ ref }
+      <Select<SelectOptionType, true> {...props}
+        ref={ref}
         isMulti
-        components={ {
+        menuPlacement='auto'
+        menuPortalTarget={document.body}
+        menuPosition='fixed'
+        components={{
           IndicatorSeparator: () => null,
-        } }
-        styles={ {
-          container: ( base ) => ( {
+          ...components
+        }}
+        styles={{
+          container: (base) => ({
             ...base,
             width: '100%',
             minHeight: '0px',
             padding: '0px'
-          } ),
-          indicatorSeparator: ( base ) => ( {
+          }),
+          indicatorSeparator: (base) => ({
             ...base,
             marginTop: '0px',
             marginBottom: '0px'
-          } ),
-          indicatorsContainer: ( base ) => ( {
+          }),
+          indicatorsContainer: (base) => ({
             ...base,
             padding: '0px',
-          } ),
-          control: ( base ) => ( {
+          }),
+          clearIndicator: (base) => ({
+            ...base,
+            padding: '0px',
+          }),
+          control: (base) => ({
             ...base,
             borderRadius: '3px',
             border: 'none',
             minHeight: '0px',
             borderColor: 'transparent',
             boxShadow: 'none'
-          } ),
-          valueContainer: ( base ) => ( {
+          }),
+          valueContainer: (base) => ({
             ...base,
             padding: '0px'
-          } ),
-          input: ( base ) => ( {
+          }),
+          input: (base) => ({
             ...base,
             padding: '0px',
             margin: '0px'
-          } ),
-          dropdownIndicator: ( base ) => ( {
+          }),
+          dropdownIndicator: (base) => ({
             ...base,
             padding: '0px',
-          } ),
-          menu: ( base ) => ( {
+          }),
+          menu: (base) => ({
             ...base,
             borderRadius: '8px',
-            marginTop: '18px'
-          } )
-        } }
+            marginTop: '18px',
+            zIndex: '999',
+            pointerEvents: 'auto'
+          }),
+          option: (base, { isSelected, isFocused, isDisabled }) => {
+            const backgroundColor = (isSelected && isFocused) ? 'hsl(var(--accent))' : undefined
+            const opacity = isDisabled ? '0.5' : '1'
+
+            return ({
+              ...base,
+              ...({ backgroundColor, opacity }),
+              color: 'black',
+            })
+          },
+          menuPortal: (base) => ({ ...base, zIndex: 999, height: 'fit-content', pointerEvents: 'auto' })
+        }}
       />
     </InputWrapper>
-  ) );
+  ));
+
+export const SelectOptionCheckBox: React.FC<OptionProps<SelectOptionType, true>> = memo(
+  ({ className, ...props }) => {
+    return (
+      <components.Option
+        className={cn("relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-2 pr-8 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50", className)}
+        {...props}
+      >
+        <Checkbox id={props.innerProps.id} label={props.label} checked={props.isSelected} onChange={() => null} onClick={() => null} />
+      </components.Option>
+    )
+  }
+)
+
+export const SelectValueContainer: React.FC<PropsWithChildren<ValueContainerProps<SelectOptionType, true> & { renderValue: (options: Options<unknown>) => ReactNode }>> = memo(({ children, renderValue, ...props }) => {
+  const containerRef = useRef<HTMLDivElement>(null)
+  const { getValue, hasValue } = props
+  const childrenLength = React.Children.count(children)
+  const lastChildren = React.Children.toArray(children)?.[childrenLength - 1] || null
+  const placeholderChildren = !hasValue && (React.Children.toArray(children)?.[0] || null)
+
+  const handleFocus = useCallback(() => {
+    if (!containerRef.current) return
+    const input = containerRef.current.querySelector('input')
+    input?.focus()
+  }, [])
+
+  return (
+    <div ref={containerRef} onClick={handleFocus}>
+      <components.ValueContainer {...props}>
+        {placeholderChildren}
+        {hasValue && renderValue ? renderValue(getValue()) : children}
+        {lastChildren}
+      </components.ValueContainer>
+    </div>
+  )
+})
 
 export default SelectMultiple;

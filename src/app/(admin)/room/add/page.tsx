b94d1b807188cfc78846f9fdd65966e2c61651d2
fide-developer <faadjarfirdaus@gmail.com>
2024-06-05 11:35:45 +0700

    Feat: Adjust room page

--- src/app/(admin)/room/add/page.tsx
+++ src/app/(admin)/room/add/page.tsx
@@ -1,27 +1,23 @@
 import { getAdmins } from '@/lib/api/admin';
-import { getCafes } from '@/lib/api/cafes';
-import { getGameList } from '@/lib/api/games';
 
 import Header from '@/components/LayoutComponents/Header';
 import PageContainer from '@/components/LayoutComponents/PageContainer';
 import AddRoomForm from '@/components/PageComponents/RoomPage/AddRoom/form';
 
 const AddRoomPage = async () => {
-  const games = await getGameList( { limit: 999999 } );
-  const admins = await getAdmins( { limit: 999999 } );
-  const cafes = await getCafes( { limit: 999999 } );
+  const admins = await getAdmins({ limit: 999999 });
 
   return (
     <>
       <Header
         title='Room Play'
-        subtitle={ [
+        subtitle={[
           { label: 'All Room Play Information', link: '/room' },
           { label: 'Add Room Play', link: '#' },
-        ] }
+        ]}
       />
       <PageContainer>
-        <AddRoomForm games={ games.data } admins={ admins.data } cafes={ cafes.data } />
+        <AddRoomForm admins={admins.data} />
       </PageContainer>
     </>
   );

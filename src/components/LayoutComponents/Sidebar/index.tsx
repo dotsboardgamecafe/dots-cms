8e75f55d8f74238a053e700c4baffdd62478a532
fide-developer <faadjarfirdaus@gmail.com>
2024-06-08 20:10:53 +0700

    feat: Fix proptypes for invoice list page and fix sidebar async component error

--- src/components/LayoutComponents/Sidebar/index.tsx
+++ src/components/LayoutComponents/Sidebar/index.tsx
@@ -3,7 +3,7 @@ import Menus from '@/components/LayoutComponents/Sidebar/Menus';
 import Brand from '@/components/ui/Brand';
 
 
-const Sidebar = async () => {
+const Sidebar = () => {
   return (
     <aside className='p-6  bg-gray-50 rounded-3xl gap-3 min-h-full'>
       <Brand />

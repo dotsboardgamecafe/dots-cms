aa0735257c3c7cfd33abd7ed4a22f40fdc6fa319
fide-developer <faadjarfirdaus@gmail.com>
2024-06-11 17:32:59 +0700

    fix: Room page table room type showing snake case data

--- src/components/PageComponents/RoomPage/DataTable/index.tsx
+++ src/components/PageComponents/RoomPage/DataTable/index.tsx
@@ -17,10 +17,12 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/Table';
 import Typography from '@/components/ui/Typography';
 
+import { snakeCaseToString } from '@/helper/string';
+
 import { Pagination as PaginationType } from '@/types/network';
 import { RoomType } from '@/types/room';
 
-dayjs.extend( dayjsFormats );
+dayjs.extend(dayjsFormats);
 
 
 type Props = {
@@ -28,19 +30,19 @@ type Props = {
   pagination: PaginationType;
 };
 
-const RoomTable = ( { data, pagination }: Props ) => {
-  const [ statusConfirmationModalOpen, setStatusConfirmationModalOpen ] = useState<boolean>( false );
-  const [ selectedRow, setSelectedRow ] = useState<RoomType>();
+const RoomTable = ({ data, pagination }: Props) => {
+  const [statusConfirmationModalOpen, setStatusConfirmationModalOpen] = useState<boolean>(false);
+  const [selectedRow, setSelectedRow] = useState<RoomType>();
   const router = useRouter();
 
-  const columns: ColumnDef<RoomType>[] = useMemo( () => [
+  const columns: ColumnDef<RoomType>[] = useMemo(() => [
     {
       accessorKey: 'type',
       header: 'Room Type',
-      cell: ( { row } ) => {
+      cell: ({ row }) => {
         return (
           <Typography variant='paragraph-l-regular' className='capitalize'>
-            { row.original.room_type }
+            {snakeCaseToString(row.original.room_type)}
           </Typography>
         );
       }
@@ -48,10 +50,10 @@ const RoomTable = ( { data, pagination }: Props ) => {
     {
       accessorKey: 'gameName',
       header: 'Game Name',
-      cell: ( { row } ) => {
+      cell: ({ row }) => {
         return (
           <Typography variant='paragraph-l-regular' className='capitalize'>
-            { row.original.name }
+            {row.original.name}
           </Typography>
         );
       }
@@ -59,10 +61,10 @@ const RoomTable = ( { data, pagination }: Props ) => {
     {
       accessorKey: 'schedule',
       header: 'Schedule',
-      cell: ( { row } ) => {
+      cell: ({ row }) => {
         return (
           <Typography variant='paragraph-l-regular'>
-            { dayjs( row.original.start_date ).format( 'ddd Do, HH:mm' ) } - { dayjs( row.original.end_date ).format( 'HH:mm' ) }
+            {dayjs(row.original.start_date).format('ddd Do, HH:mm')} - {dayjs(row.original.end_date).format('HH:mm')}
           </Typography>
         );
       }
@@ -70,10 +72,10 @@ const RoomTable = ( { data, pagination }: Props ) => {
     {
       accessorKey: 'location',
       header: 'Location',
-      cell: ( { row } ) => {
+      cell: ({ row }) => {
         return (
           <Typography variant='paragraph-l-regular'>
-            { row.original.cafe_name }
+            {row.original.cafe_name}
           </Typography>
         );
       }
@@ -81,10 +83,10 @@ const RoomTable = ( { data, pagination }: Props ) => {
     {
       accessorKey: 'level',
       header: 'Level',
-      cell: ( { row } ) => {
+      cell: ({ row }) => {
         return (
           <Typography variant='paragraph-l-regular' className='capitalize'>
-            { row.original.difficulty }
+            {row.original.difficulty}
           </Typography>
         );
       }
@@ -92,10 +94,10 @@ const RoomTable = ( { data, pagination }: Props ) => {
     {
       accessorKey: 'slot',
       header: 'Updated Slot',
-      cell: ( { row } ) => {
+      cell: ({ row }) => {
         return (
           <Typography variant='paragraph-l-regular'>
-            { row.original.current_used_slot } / { row.original.maximum_participant }
+            {row.original.current_used_slot} / {row.original.maximum_participant}
           </Typography>
         );
       }
@@ -103,12 +105,12 @@ const RoomTable = ( { data, pagination }: Props ) => {
     {
       accessorKey: 'gameMaster',
       header: 'Game Master',
-      cell: ( { row } ) => {
+      cell: ({ row }) => {
         return (
           <div className='flex flex-row items-center gap-[10px]'>
-            <Image width={ 36 } height={ 36 } src={ row.original.game_master_image_url || '/images/avatar-not-found.png' } alt='profile image' className='rounded-full' />
+            <Image width={36} height={36} src={row.original.game_master_image_url || '/images/avatar-not-found.png'} alt='profile image' className='rounded-full' />
             <Typography variant='paragraph-l-regular'>
-              { row.original.game_master_name }
+              {row.original.game_master_name}
             </Typography>
           </div>
         );
@@ -116,34 +118,34 @@ const RoomTable = ( { data, pagination }: Props ) => {
     },
     {
       accessorKey: 'status',
-      accessorFn: ( row ) => row.status,
+      accessorFn: (row) => row.status,
       header: 'Status',
-      cell: ( { row } ) => {
+      cell: ({ row }) => {
         return (
-          <Select value={ row.original.status }
-            disabled={ row.original.status === 'inactive' || row.original.current_used_slot > 0 }
-            onValueChange={ ( value ) => {
-              if ( value === 'inactive' ) {
-                setStatusConfirmationModalOpen( true );
-                setSelectedRow( row.original );
+          <Select value={row.original.status}
+            disabled={row.original.status === 'inactive' || row.original.current_used_slot > 0}
+            onValueChange={(value) => {
+              if (value === 'inactive') {
+                setStatusConfirmationModalOpen(true);
+                setSelectedRow(row.original);
               }
-            } }
+            }}
           >
-            <SelectTrigger variant='badge' className={ cn(
+            <SelectTrigger variant='badge' className={cn(
               {
                 'bg-error-50': row.original.status === 'inactive',
                 'bg-blue-50': row.original.status === 'active'
               }
-            ) }>
-              <SelectValue aria-label={ row.original.status }>
-                <Typography variant='text-body-l-medium' className={ cn(
+            )}>
+              <SelectValue aria-label={row.original.status}>
+                <Typography variant='text-body-l-medium' className={cn(
                   'capitalize',
                   {
                     'text-error-700': row.original.status === 'inactive',
                     'text-blue-700': row.original.status === 'active'
                   }
-                ) }>
-                  { row.original.status }
+                )}>
+                  {row.original.status}
                 </Typography>
               </SelectValue>
             </SelectTrigger>
@@ -158,13 +160,13 @@ const RoomTable = ( { data, pagination }: Props ) => {
     {
       id: 'action',
       header: 'Action',
-      cell: ( { row } ) => {
+      cell: ({ row }) => {
         return (
           <div className='flex flex-row items-center gap-4'>
-            <Link href={ `/room/view/${row.original.room_code}` } >
+            <Link href={`/room/view/${row.original.room_code}`} >
               <Eye className='cursor-pointer' />
             </Link>
-            <Link href={ `/room/edit/${row.original.room_code}` } >
+            <Link href={`/room/edit/${row.original.room_code}`} >
               <Edit className='cursor-pointer' />
             </Link>
           </div>
@@ -172,19 +174,19 @@ const RoomTable = ( { data, pagination }: Props ) => {
       }
     }
   ]
-    , [] );
+    , []);
 
 
-  const table = useReactTable( {
+  const table = useReactTable({
     data: data,
     getCoreRowModel: getCoreRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
     columns,
-  } );
+  });
 
   const getMaxPage = () => {
-    if ( pagination.count && pagination.limit ) {
-      return Math.ceil( pagination?.count / pagination?.limit );
+    if (pagination.count && pagination.limit) {
+      return Math.ceil(pagination?.count / pagination?.limit);
     }
     return 1;
   };
@@ -206,68 +208,68 @@ const RoomTable = ( { data, pagination }: Props ) => {
       <Table>
         <TableHeader>
           {
-            table.getHeaderGroups().map( ( headerGroup ) => (
-              <TableRow key={ headerGroup.id }>
-                { headerGroup.headers.map( ( header ) => {
+            table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
                   return (
-                    <TableHead key={ header.id }>
-                      { header.isPlaceholder
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
                         ? null
                         : flexRender(
                           header.column.columnDef.header,
                           header.getContext()
-                        ) }
+                        )}
                     </TableHead>
                   );
-                } ) }
+                })}
               </TableRow>
-            ) )
+            ))
           }
         </TableHeader>
         <TableBody>
-          { table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map( ( row ) => (
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
               <TableRow
-                key={ row.id }
-                data-state={ row.getIsSelected() && "selected" }
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
               >
-                { row.getVisibleCells().map( ( cell ) => (
-                  <TableCell key={ cell.id }>
-                    { flexRender( cell.column.columnDef.cell, cell.getContext() ) }
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
                   </TableCell>
-                ) ) }
+                ))}
               </TableRow>
-            ) )
+            ))
           ) : (
             <TableRow>
-              <TableCell colSpan={ columns.length } className="h-24 text-center">
+              <TableCell colSpan={columns.length} className="h-24 text-center">
                 No results.
               </TableCell>
             </TableRow>
-          ) }
+          )}
         </TableBody>
       </Table>
       <PaginationDeprecated
-        totalPages={ getMaxPage() }
-        currentPage={ pagination.page }
-        itemsPerPage={ pagination.limit ?? 0 }
-        totalItems={ pagination.count ?? 0 }
-        onChangeItemsPerPage={ ( items ) => {
-          router.push( `/room?page=1&limit=${items}` );
-          table.setPageSize( items );
-        } }
-        onNext={ () => router.push( `/room?page=${( pagination.page ?? 1 ) + 1}` ) }
-        onPrevious={ () => router.push( `/room?page=${( pagination.page ?? 1 ) - 1}` ) }
-        prevDisabled={ pagination.page === 1 }
-        nextDisabled={ pagination.page === getMaxPage() }
-        onChangePage={ ( page ) => router.push( `/room?page=${page}` ) }
-        from={ table.getState().pagination.pageIndex * table.getState().pagination.pageSize + 1 }
-        to={ table.getState().pagination.pageIndex * table.getState().pagination.pageSize + table.getState().pagination.pageSize }
+        totalPages={getMaxPage()}
+        currentPage={pagination.page}
+        itemsPerPage={pagination.limit ?? 0}
+        totalItems={pagination.count ?? 0}
+        onChangeItemsPerPage={(items) => {
+          router.push(`/room?page=1&limit=${items}`);
+          table.setPageSize(items);
+        }}
+        onNext={() => router.push(`/room?page=${(pagination.page ?? 1) + 1}`)}
+        onPrevious={() => router.push(`/room?page=${(pagination.page ?? 1) - 1}`)}
+        prevDisabled={pagination.page === 1}
+        nextDisabled={pagination.page === getMaxPage()}
+        onChangePage={(page) => router.push(`/room?page=${page}`)}
+        from={table.getState().pagination.pageIndex * table.getState().pagination.pageSize + 1}
+        to={table.getState().pagination.pageIndex * table.getState().pagination.pageSize + table.getState().pagination.pageSize}
       />
       <StatusConfirmationModal
-        open={ statusConfirmationModalOpen }
-        onOpenChange={ ( value ) => setStatusConfirmationModalOpen( value ) }
-        roomData={ selectedRow }
+        open={statusConfirmationModalOpen}
+        onOpenChange={(value) => setStatusConfirmationModalOpen(value)}
+        roomData={selectedRow}
       />
     </div>
   );

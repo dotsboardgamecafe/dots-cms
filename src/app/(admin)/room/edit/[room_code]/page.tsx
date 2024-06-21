ce3bc5902f275a04de27c3667fe2a4ace305f744
fide-developer <faadjarfirdaus@gmail.com>
2024-06-22 00:09:44 +0700

    feat: Adjust the game selection on the room add/edit to be searchable

--- src/app/(admin)/room/edit/[room_code]/page.tsx
+++ src/app/(admin)/room/edit/[room_code]/page.tsx
@@ -1,4 +1,3 @@
-import { getAdmins } from '@/lib/api/admin';
 import { getRoomDetail } from '@/lib/api/room';
 
 import Header from '@/components/LayoutComponents/Header';
@@ -6,7 +5,6 @@ import PageContainer from '@/components/LayoutComponents/PageContainer';
 import EditRoomForm from '@/components/PageComponents/RoomPage/EditRoom';
 
 const AddRoomPage = async ({ params: { room_code } }: { params: { room_code: string; }; }) => {
-  const admins = await getAdmins({ limit: 999999 });
   const roomDetail = await getRoomDetail({ param: room_code });
   return (
     <>
@@ -18,7 +16,7 @@ const AddRoomPage = async ({ params: { room_code } }: { params: { room_code: str
         ]}
       />
       <PageContainer>
-        <EditRoomForm admins={admins.data} roomDetail={roomDetail.data} />
+        <EditRoomForm roomDetail={roomDetail.data} />
       </PageContainer>
     </>
   );

73175236920be07159efd7bedbb188d77c5184a7
fide-developer <faadjarfirdaus@gmail.com>
2024-06-22 01:46:24 +0700

    fix: modal overflow on the member page

--- src/components/PageComponents/MemberPage/DetailModal/index.tsx
+++ src/components/PageComponents/MemberPage/DetailModal/index.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import { PropsWithRef } from 'react';
 
 import { Modal, ModalContent } from '@/components/ui/Modal';
-import { Separator } from '@/components/ui/Separator';
+import TextLabel from '@/components/ui/TextLabel';
 import Typography from '@/components/ui/Typography';
 
 import { MemberType } from '@/types/member';
@@ -18,60 +18,20 @@ type Props = PropsWithRef<{
 const MemberDetailModal = ({ open, onOpenChange, memberData }: Props) => {
   return (
     <Modal open={open} onOpenChange={onOpenChange} >
-      <ModalContent hideCloseIcon>
-        <div className='gap-6 flex flex-col'>
+      <ModalContent hideCloseIcon className='max-h-[90%] overflow-hidden flex flex-col'>
+        <div className='gap-6 flex flex-col overflow-hidden'>
           <section className='flex flex-row items-center gap-3'>
             <Image src={memberData?.image_url || '/images/avatar-not-found.png'} alt="member-detail-avatar" width={52} height={52} className='rounded-full' />
             <Typography variant='heading-h4'>
               {memberData?.fullname}
             </Typography>
           </section>
-          <section className='flex flex-col  gap-1'>
-            <Typography variant='paragraph-l-regular'>
-              Username
-            </Typography>
-            <Typography variant='paragraph-xl-regular'>
-              {memberData?.username}
-            </Typography>
-            <Separator className='mt-1' />
-          </section>
-          <section className='flex flex-col  gap-1'>
-            <Typography variant='paragraph-l-regular'>
-              Email Address
-            </Typography>
-            <Typography variant='paragraph-xl-regular'>
-              {memberData?.email}
-            </Typography>
-            <Separator className='mt-1' />
-          </section>
-          <section className='flex flex-col  gap-1'>
-            <Typography variant='paragraph-l-regular'>
-              Phone Number
-            </Typography>
-            <Typography variant='paragraph-xl-regular'>
-              {memberData?.phone_number}
-            </Typography>
-            <Separator className='mt-1' />
-          </section>
-          <section className='flex flex-col  gap-1'>
-            <Typography variant='paragraph-l-regular'>
-              Tier Level
-            </Typography>
-            <Typography variant='paragraph-xl-regular'>
-              {memberData?.latest_tier || '-'}
-            </Typography>
-            <Separator className='mt-1' />
-          </section>
-          <section className='flex flex-col  gap-1'>
-            <Typography variant='paragraph-l-regular'>
-              Status
-            </Typography>
-            <Typography variant='paragraph-xl-regular' className='capitalize'>
-              {memberData?.status}
-            </Typography>
-            <Separator className='mt-1' />
-          </section>
-          <section>
+          <section className='flex flex-col gap-6 flex-grow overflow-y-auto'>
+            <TextLabel title='Username' value={memberData?.username} />
+            <TextLabel title='Email Address' value={memberData?.email} />
+            <TextLabel title='Phone Number' value={memberData?.phone_number} />
+            <TextLabel title='Tier Level' value={memberData?.latest_tier} />
+            <TextLabel title='Status' value={memberData?.status} className='capitalize' />
             <Link href={`/member/invoices/${memberData?.user_code}`}>
               <Typography variant='text-body-l-regular' className='text-brand-blue-electric'>
                 View invoice history

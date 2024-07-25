91b723acc73ac05407724b3417b5957c5c110dbc
fide-developer <faadjarfirdaus@gmail.com>
2024-07-25 20:49:12 +0700

    feat: Room details - Adjust the schedule format

--- src/helper/datetime.ts
+++ src/helper/datetime.ts
@@ -4,7 +4,7 @@ import dayjsFormats from 'dayjs/plugin/advancedFormat';
 dayjs.extend(dayjsFormats);
 
 export const formatRoomSchedule = (start_date: string, end_date: string, start_time: string, end_time: string) => {
-  return `${dayjs(start_date).format('ddd Do, ') + start_time} - ${end_time}`;
+  return `${dayjs(start_date).format('ddd, DD MMM YYYY')} ${formatTimeHourMinutes(start_time)} - ${formatTimeHourMinutes(end_time)}`;
 };
 
 export const formatTournamentDate = (start_date: string, end_date: string): string => {
@@ -28,7 +28,7 @@ export const formatDate = (date: string) => {
   return dayjs(date).format('MMM Do, YYYY');
 };
 
-export const formatTimeHourMinutes = (time: string): string => {
+export function formatTimeHourMinutes(time: string): string {
   const splitTime: string[] = time.split(':')
   const [hour, minute] = splitTime
 

e18cde31f3ba74dc551ad3fe59d26f4a51c74aa1
fide-developer <faadjarfirdaus@gmail.com>
2024-07-31 22:51:15 +0700

    feat: tournament page - add filter by status and change the default filter to show only active tournament

--- src/app/(admin)/tournament/page.tsx
+++ src/app/(admin)/tournament/page.tsx
@@ -9,9 +9,14 @@ import PageContainer from '@/components/LayoutComponents/PageContainer';
 import TournamentTable from '@/components/PageComponents/TournamentPage/DataTable';
 
 import { PageProps } from '@/types/common';
+import { Pagination } from '@/types/network';
 
 const RoomPage = async ({ searchParams }: PageProps) => {
-  const tournaments = await getTournaments({ pagination: { order: 'tournaments.status,tournaments.created_date', sort: 'ASC,DESC', ...searchParams } });
+  const isFilterAll: boolean = searchParams.status === 'all'
+  const pagination: Pagination = { order: 'tournaments.status,tournaments.created_date', sort: 'ASC,DESC', status: 'active', ...searchParams }
+  if (isFilterAll) delete pagination.status
+
+  const tournaments = await getTournaments({ pagination: pagination });
   return (
     <>
       <Header title='Tournament' subtitle={[{

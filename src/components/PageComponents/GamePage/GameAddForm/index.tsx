2dc6017c33656f10cc723d731aacbb7dded57071
fide-developer <faadjarfirdaus@gmail.com>
2024-08-29 11:09:09 +0700

    feat: game page - removing the limit 100 characters on the description field

--- src/components/PageComponents/GamePage/GameAddForm/index.tsx
+++ src/components/PageComponents/GamePage/GameAddForm/index.tsx
@@ -337,7 +337,7 @@ const AddGameForm = ({ cafes, admins }: Props) => {
                         </Typography>
                       </FormLabel>
                       <FormControl>
-                        <Textarea note='Max 100 characters' maxLength={100} placeholder='Game Description...' className='min-h-[200px]' {...field} />
+                        <Textarea placeholder='Game Description...' className='min-h-[200px]' {...field} />
                       </FormControl>
                       <FormMessage />
                     </FormItem>

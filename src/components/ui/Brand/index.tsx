afd44f2252d2c472f823bdfe7bcfefc9c813c1f3
fide-developer <faadjarfirdaus@gmail.com>
2024-09-02 13:57:44 +0700

    feat: adjust logo

--- src/components/ui/Brand/index.tsx
+++ src/components/ui/Brand/index.tsx
@@ -7,7 +7,7 @@ const Brand = () => {
   return (
     <>
       <div className='flex flex-row items-center gap-3 mb-[30px]'>
-        <Image src="/images/logo-dots.png" alt="brand" width={ 38 } height={ 38 } style={ { width: 'auto', height: 'auto' } } />
+        <Image src="/images/logo-dots.png" alt="brand" width={38} height={38} style={{ width: 'auto', height: 'auto' }} />
         <Typography variant='bubble-heading-h2'>
           DOTS
         </Typography>

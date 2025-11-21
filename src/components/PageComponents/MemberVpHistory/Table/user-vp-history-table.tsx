58688ad0e5cdf6af933edd413f166c3844c003e8
fide-developer <faadjarfirdaus@gmail.com>
2025-11-21 21:41:00 +0700

    feat: show time on the vp history table (created date column)

--- src/components/PageComponents/MemberVpHistory/Table/user-vp-history-table.tsx
+++ src/components/PageComponents/MemberVpHistory/Table/user-vp-history-table.tsx
@@ -25,7 +25,7 @@ const UserVpHistoryTable = ({ data, pagination, isLoading }: Props) => {
         cell: ({ row }) => {
           return (
             <Typography variant='paragraph-l-regular'>
-              {row.original.created_date ? dayjs(row.original.created_date).format('DD MMM, YYYY') : 'n/a'}
+              {row.original.created_date ? dayjs(row.original.created_date).format('DD MMM, YYYY HH:mm a') : 'n/a'}
             </Typography>
           );
         }
